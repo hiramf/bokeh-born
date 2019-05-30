6e404dd595dc4d47cecf41ed35e8d5b58e3354fa
Hiram Foster <hfoster@prevedere.com>
2019-05-29 22:29:58 -0400

    add residual plot example

--- example.py
+++ example.py
@@ -11,12 +11,13 @@ from bokeh.plotting import show
 periods = 50
 df1 = pd.DataFrame(np.random.rand(periods,6))
 df1.columns =  ['a', 'b', 'c', 'd', 'e', 'f']
+residuals = np.random.randn(periods)
 
 # Correlation Matrix
-show(sbk.matrix_figure(df))
+show(sbk.matrix_figure(df1))
 
 # Residuals over Time
-df2 = pd.DataFrame({'e': np.random.randn(periods)}, 
+df2 = pd.DataFrame({'e': residuals}, 
                    index=pd.date_range(start='2015-01-01', 
                                        periods=periods, 
                                        freq='MS', 
@@ -27,3 +28,8 @@ show(sbk.residuals_time(df2))
 
 # Distribution Plot
 show(sbk.distplot(df['e']))
+
+# Residuals Plot
+yhat = np.random.randn(periods)
+
+show(sbk.residplot(yhat=yhat, e=residuals))
